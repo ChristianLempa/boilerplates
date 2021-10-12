d47a0d66a47ff7137c110e1dee284187fac2f4e4
xcad2k <28359525+xcad2k@users.noreply.github.com>
2021-10-12 10:29:44 +0200

    terraform update

--- terraform/examples/cloud-deployment-example/main.tf
+++ terraform/examples/cloud-deployment-example/main.tf
@@ -1,4 +1,6 @@
-# For more information, see https://www.terraform.io/docs/backends/types/remote.html
+# General Terraform Settings
+# ---
+
 terraform {
   required_providers {
     cloudflare = {
@@ -11,7 +13,28 @@ terraform {
   }
 }
 
+# Declare Variables
+# ---
+# TODO: Create a yourfile.auto.tfvars file in the project directory and add your variables in it.
+#   Example:
+#   cloudflare_email = "youremail@yourmail.com"
+#   cloudflare_api_key = "your-api-key"
+#   civo_token = "your-token"
+
+variable "cloudflare_email" {}
+variable "cloudflare_api_key" {}
+variable "civo_token" {}
+
+# Set Default Provider Settings
+# ---
+
 provider "cloudflare" {
-    email = var.cloudflare_email
-    api_key =  var.cloudflare_api_key
+  email = var.cloudflare_email
+  api_key =  var.cloudflare_api_key
+}
+
+provider "civo" {
+  token = var.civo_token
+  # (optional) change the defaullt region
+  # region = "FRA1"
 }
