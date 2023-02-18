2fe82bfeb1c5ce1d4a38591964bfa606cbd2f160
scan-dev <scan@vmlab.be>
2023-02-18 19:32:50 +0100

    add readme python

--- python/create-ui/create-simple-ui.py
+++ python/create-ui/create-simple-ui.py
@@ -1,4 +1,5 @@
 import customtkinter
+#import tkinter
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
@@ -12,19 +13,19 @@ def login():
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
 
-label = customtkinter.CTkLabel(master=frame, text="Login System", text_font=("Roboto, 24"))
-label.pack(pady12, padx=10)
+label = customtkinter.CTkLabel(master=frame, text="Login System")
+label.pack(pady=12, padx=10)
 
 entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
-entry1.pack(pady12, padx=10)
+entry1.pack(pady=12, padx=10)
 
-enty2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
-entry2.pack(pady12, padx=10)
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
 
-button = customtkinter.CTkButton(master=frame, placeholder_text="Username")
-button.pack(pady12, padx=10)
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
 
-checkbox = customtkinter.CTkCheckbox(master=frame, text="Remember me")
-checkbox.pack(pady12, padx=10)
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember me")
+checkbox.pack(pady=12, padx=10)
 
 root.mainloop()
