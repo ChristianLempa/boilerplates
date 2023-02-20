9eac8df83b8a21476cf0b1b6f56463b5c3c21693
scan-dev <scan@vmlab.be>
2023-02-20 01:58:16 +0100

    update main.py

--- python/dev-tool/src/main.py
+++ python/dev-tool/src/main.py
@@ -2,12 +2,42 @@ from fastapi import FastAPI
 from datetime import datetime
 import redis
 
-app = FastAPI()
+version = "1.0.0"
+title = "FastAPI Container-based Code Example"
+description = """
+FastAPI Container-based example
+
+## ITEMS
+
+You can see **hit** counts and some stats
+
+## USERS
+
+You will be able to:
+
+* **See hello-world**
+* **See number of hits**
+* **See Latest-hit**
+"""
+contact ="info@vmlab.be"
+
+app = FastAPI(
+    title = "FastAPI Container-based Code Example",
+    description= description,
+    version = "1.0.0",
+    contact={
+      "name": "VMLAB",
+      "url": "https://vmlab.be",
+      "email": "info@vmlab.be",
+    },
+)
+
 r = redis.Redis(host="redis", port=6379)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World1234"}
+    r.incr("hits")
+    return {"Hello": "World!"}
 
 @app.get("/hits")
 def read_root():
