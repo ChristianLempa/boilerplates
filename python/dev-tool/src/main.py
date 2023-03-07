e0534af57bdf436c2a05ac6d4fa953c0a8d0ad6b
bartcocquyt <bart.cocquyt@zf.com>
2023-03-07 15:33:48 +0100

    update all local changes

--- python/dev-tool/src/main.py
+++ python/dev-tool/src/main.py
@@ -26,7 +26,27 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from pydantic import BaseModel, Field
 
+## App defenition
+version = "1.0.0"
+title = "FastAPI Container-based Code Example"
+description = """
+FastAPI Container-based example
 
+## ITEMS
+
+You can see **hit** counts and some stats
+
+## USERS
+
+You will be able to:
+
+* **See status**
+* **Get employees by ID**
+* **Post employes by Json**
+"""
+contact ="info@vmlab.be"
+
+## end Defenition
 class Notification(BaseModel):
     email: str
     notification_type: int
@@ -41,41 +61,57 @@ class Department(str, Enum):
 
 class Employee(BaseModel):
     id: int = Field(description="Employee ID")
+    name: str = Field(max_length=40, description="Name of the employee")
     department: Department = Field(
         description="The department the employee belongs to.",
     )
-    age: int = Field(description="The age of the employee")
-    gender: str = Field(max_length=1, description="The gender of the employee")
+    age: int = Field(description="Age of the employee")
+    gender: str = Field(max_length=1, description="Gender of the employee")
 
+employees_dB = redis.Redis(host="redis", port=6379)
+e = employees_dB.set[{"id": 1, "name": "Bob", "department": "math", "age": 49, "gender": "m"}, {"id": 2, "name": "Mike", "department": "chemistry", "age": 50, "gender": "m"}, {"id": 3, "name": "Elise", "department": "english", "age": 51, "gender": "f"}]
 
-fake_employees_db = []
-
-app = FastAPI(debug=True)
-
-origins = [
-    "http://localhost:5000",
-]
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+app = FastAPI(
+    title = "FastAPI Container-based Code Example",
+    description= description,
+    version = "1.0.0",
+    contact={
+      "name": "VMLAB",
+      "url": "https://vmlab.be",
+      "email": "info@vmlab.be",
+    },
+    debug=True
 )
+
+#origins = [
+#    "http://localhost:5000",
+#]
+#app.add_middleware(
+#    CORSMiddleware,
+#    allow_origins=origins,
+#    allow_credentials=True,
+#    allow_methods=["*"],
+#    allow_headers=["*"],
+#)
 
 
 @app.get("/status")
 async def check_status():
-    return "Hello World"
+    return "Healthy"
 
 
 @app.get(
     "/employees/{employee_id}", response_model=Employee, status_code=status.HTTP_200_OK
 )
 async def get_employees(
-    employee_id: int, age: int, department: Department, gender: str = None
+    employee_id: int, department: str = None, age: int = None, gender: str = None
 ):
-    return [{"id": 1, "name": "Bob"}, {"id": 2, "name": "Mike"}]
+    
+    return fake_employees_db(employee_id)
+    
+#    [{"id": 1, "department": "math", "age": "49", "gender": "m"}]
+#     {"id": 2, "department": "math", "age": "49""name": "Mike","gender": "male"},
+#     {"id": 3, "department": "math", "name": "Elise","gender": "female"}]
 
 
 @app.post("/employees", response_model=Employee, status_code=status.HTTP_201_CREATED)
@@ -104,4 +140,4 @@ async def send_email(
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080, reload=False)
+    uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
