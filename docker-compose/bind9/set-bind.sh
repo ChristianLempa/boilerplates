e9aac746fa42c104a7722f714fa9b75f5c1744ea
Ben Sparks <bsparks@dollargeneral.com>
2023-11-30 11:12:17 -0600

    add ability to run as non root user

---
+++ docker-compose/bind9/set-bind.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# before running the docker image, run this script to set the
+# uid to 100 and gid to 101. This is based on the bind user in the Ubuntu docker image
+#:/# grep bind /etc/passwd
+#bind:x:100:101::/var/cache/bind:/usr/sbin/nologin
+#
+sudo rndc-confgen >./config/rndc.key
+chown 100:101 -R cache
+chown 100:101 -R config
+chown 100:101 -R records
