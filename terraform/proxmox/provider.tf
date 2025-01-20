31c9df7752e2d303f82cb0f3ed0843155e60e8b7
Christian Lempa <christian.lempa@clcreative.de>
2025-01-20 17:21:41 +0100

    updated proxmox vm template

--- terraform/proxmox/provider.tf
+++ terraform/proxmox/provider.tf
@@ -3,36 +3,39 @@
 # Initial Provider Configuration for Proxmox
 
 terraform {
+  required_version = ">= 0.13.0"
 
-    required_version = ">= 0.13.0"
-
-    required_providers {
-        proxmox = {
-            source = "telmate/proxmox"
-            version = ">= 2.9.14"
-        }
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+      version = "3.0.1-rc6"
     }
+  }
 }
 
 variable "proxmox_api_url" {
-    type = string
+  type = string
 }
 
 variable "proxmox_api_token_id" {
-    type = string
+  type = string
 }
 
 variable "proxmox_api_token_secret" {
-    type = string
+  type = string
+}
+
+variable "PUBLIC_SSH_KEY" {
+  
+  # -- Public SSH Key, you want to upload to VMs and LXC containers.
+
+  type = string
+  sensitive = true
 }
 
 provider "proxmox" {
-
-    pm_api_url = var.proxmox_api_url
-    pm_api_token_id = var.proxmox_api_token_id
-    pm_api_token_secret = var.proxmox_api_token_secret
-
-    # (Optional) Skip TLS Verification
-    # pm_tls_insecure = true
-
+  pm_api_url = var.proxmox_api_url
+  pm_api_token_id = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+  pm_tls_insecure = false  # <-- (Optional) Change to true if you are using self-signed certificates
 }
