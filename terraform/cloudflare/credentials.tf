ce1a25f28df71dc0fd7b29109616c75ca1393753
Avnit Bambah <pocuser@satisfilabs.com>
2025-02-23 22:15:38 -0800

    adding map to the terraform code

--- terraform/cloudflare/credentials.tf
+++ terraform/cloudflare/credentials.tf
@@ -11,3 +11,27 @@ variable "cloudflare_api_key" {
     description = "The API key for your Cloudflare account"
     type = string
 }
+variable "cloudflare_zone_id" {
+    description = "The Zone ID for your Cloudflare account"
+    type = string
+}
+variable "dns_record_name" {
+    description = "The name of the DNS record you want to create"
+    type = map(string)
+    default = {
+        ubuntu  = {
+            name = "ubuntu"
+            value = "192.168.68.65:8080"
+            type = "A"
+            ttl = 1
+            proxied = true
+        }
+        jellyfyn  = {
+            name = "jellyfyn"
+            value = ""
+            type = "A"
+            ttl = 1
+            proxied = true
+        }            
+    }
+}
