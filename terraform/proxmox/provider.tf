947d0f51d2081662834507804c6cbc13904f282a
Christian Lempa <christian.lempa@clcreative.de>
2025-03-31 16:49:04 +0200

    space

--- terraform/proxmox/provider.tf
+++ terraform/proxmox/provider.tf
@@ -34,6 +34,7 @@ provider "proxmox" {
   pm_api_url = var.PROXMOX_URL
   pm_api_token_id = var.PROXMOX_USER
   pm_api_token_secret = var.PROXMOX_TOKEN
+  
   # NOTE Optional, but recommended to set to true if you are using self-signed certificates.
   pm_tls_insecure = false
 }
