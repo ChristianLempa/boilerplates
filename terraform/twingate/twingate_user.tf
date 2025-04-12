fbcfb041746ccc4b335a18a6856ff254564b85ae
xcad <christian.lempa@clcreative.de>
2025-04-12 17:56:54 +0200

    fix comments

--- terraform/twingate/twingate_user.tf
+++ terraform/twingate/twingate_user.tf
@@ -6,7 +6,6 @@ resource "twingate_user" "new_user" {
   email       = "new.user@example.com"
   first_name  = "New"
   last_name   = "User"
-  role        = "DEVOPS" # NOTE Defines the role, either ADMIN, DEVOPS,
-  # SUPPORT or MEMBER
+  role        = "DEVOPS" # NOTE Defines the role, either ADMIN, DEVOPS, SUPPORT or MEMBER
   send_invite = true
 }
